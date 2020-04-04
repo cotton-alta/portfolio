1d7989b614e69d0c3265f74945c5fdce3190c2ee
cotton <chinoknct@gmail.com>
2020-04-04 18:22:48 +0900

    [add]トップページにタイトルを追加
    トップページのサンプルロゴを削除してタイトルを追加しました。

---
+++ client/components/layouts/topTitle.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="content">
+    <div class="title">
+      <span>cotton's portfolio</span>
+    </div>
+    <!-- /.title -->
+  </div>
+  <!-- /.container -->
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.content {
+  height: 100px;
+  line-height: 100px;
+}
+
+.title {
+  text-align: center;
+  span {
+    font-size: calc(35px + 1.5vw);
+    font-weight: bolder;
+    color: #ffffff;
+  }
+}
+</style>
