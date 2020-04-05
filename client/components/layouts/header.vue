d158e655c6f32fa1ce98f64941a5e44723ccc89d
cotton <chinoknct@gmail.com>
2020-04-05 13:31:47 +0900

    [add]ヘッダーの追加およびsass-mqの導入
    ヘッダーを追加したのとレスポンシブに対応するためにsass-mqを導入しました。

--- client/components/layouts/header.vue
+++ client/components/layouts/header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="header-wrapper">
     <div v-for="data in datas" :key="data.id" class="header-card">
-      <nuxt-link :to="data.href">{{ data.title }}</nuxt-link>
+      <nuxt-link class="header-name" :to="data.href">{{ data.title }}</nuxt-link>
     </div>
   </div>
 </template>
@@ -25,19 +25,31 @@ export default {
 
 <style lang="scss" scoped>
 
-.header-wrapper {
+.header {
   &-wrapper {
-    height: 80px;
-    width: 100%;
-    background-color: $menu-background;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
+    display: none;
+    @include mq {
+      height: 80px;
+      line-height: 80px;
+      width: 100%;
+      background-color: $menu-background;
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      justify-items: center;
+    }
   }
   &-card {
-    height: 100%;
+    display: inline-block;
+    margin: 0 15px;
+    vertical-align: middle;
   }
-
+  &-name {
+    text-decoration: none;
+    font-size: calc(20px + 0.8vw);
+    font-weight: bolder;
+    color: $main-background;
+  }
 }
 
 </style>
